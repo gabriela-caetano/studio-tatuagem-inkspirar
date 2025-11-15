5faeaa4787bf5f64bda9b515af9de33650357bf6
Gabriela Aparecida Silva Caetano <166453006+gabriela-caetano@users.noreply.github.com>
2025-11-15 15:12:36 -0300

    Create database.js

---
+++ backend/config/database.js
@@ -0,0 +1,14 @@
+// backend/config/database.js
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'studio_tatuagem_inkspirar',
+  waitForConnections: true,
+  connectionLimit: 10,
+});
+
+module.exports = pool;
